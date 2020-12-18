26ca2f1f52735a62ba85f72d2bcd0b30ff321066
Edyta Pałka <65342415+KiroBunny@users.noreply.github.com>
2020-12-18 11:48:19 +0100

    Update KsiazkaTelefoniczna.java

--- ksiazkatelefoniczna/KsiazkaTelefoniczna.java
+++ ksiazkatelefoniczna/KsiazkaTelefoniczna.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ksiazkatelefoniczna;
 
 import java.util.*;
 
-/**
- *
- * @author Marcin
- */
 public class KsiazkaTelefoniczna implements IKsiazkaTelefoniczna {
 
     Map<Integer, IOsoba> id2Osoba = new HashMap<>(); //klucz musi posiadac dobrze zaimplementowane metody porownania
