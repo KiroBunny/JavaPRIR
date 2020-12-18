9f614d9332d8498045153edb6266c0df48a96cb0
Edyta Pałka <65342415+KiroBunny@users.noreply.github.com>
2020-12-18 11:48:49 +0100

    Update Main.java

--- ksiazkatelefoniczna/Main.java
+++ ksiazkatelefoniczna/Main.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ksiazkatelefoniczna;
 
 import java.util.Collection;
-
-/**
- *
- * @author Marcin
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         IKsiazkaTelefoniczna ksiazkaTelefoniczna = new KsiazkaTelefoniczna();
         IOsoba o1, o2, o3;
