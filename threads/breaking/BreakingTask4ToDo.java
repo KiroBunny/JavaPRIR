a23973b2c15cf4fb16bd0ea0070688adc2e01fbe
Edyta Pałka <65342415+KiroBunny@users.noreply.github.com>
2020-12-18 11:51:34 +0100

    Update BreakingTask4ToDo.java

--- threads/breaking/BreakingTask4ToDo.java
+++ threads/breaking/BreakingTask4ToDo.java
@@ -6,11 +6,6 @@ import java.util.Random;
 
 import static java.lang.Thread.sleep;
 
-/**
- * Zmodyfikuj metodę run, generateMap oraz maxRoznica w klasie BreakingTask2ToDo tak aby po wystąpieniu przerwaniu wątka, wątek zakończył swoją
- * pracę. Zadanie wykonaj tak, aby po wystąpieniu przerwania wątku na ekranie nie pojawiły się informacje o kolejnych iteracjach wątku,
- * Zakończenie wątku powinno wystąpić jak najszybciej po wsytąpieniu sygnału interrupt.
- */
 
 public class BreakingTask4ToDo implements Runnable {
     int counter = 0;
@@ -25,10 +20,6 @@ public class BreakingTask4ToDo implements Runnable {
     }
 
 
-    /**
-     * Metoda wątku -  w nieskończonej pętli generowana jest nowa mapa częstości, a następnie porównywane są różnice częstości
-     * Tutaj również znależy umieścić procedurę zakończenia wątku.
-     */
     @Override
     public void run() {
 
@@ -45,13 +36,6 @@ public class BreakingTask4ToDo implements Runnable {
         }
     }
 
-    /**
-     * Metoda generuje mapę, w której kluczem jest string reprezentujący liczbę z przedziału 0-9, a wartością jest licznik
-     * wskazujący ile razy dana wartość wystąpiła.
-     * Aby jak najszybciej zakończyć wątek należy przerwać wykonywanie pętli
-     *
-     * @param map
-     */
     public void generatemMap(Map<String, Integer> map) {
         Random r = new Random();
         System.out.println("generateMap: " + counter);
@@ -69,15 +53,6 @@ public class BreakingTask4ToDo implements Runnable {
         }
     }
 
-    /**
-     * Metoda zwraca największą różnicę pomiędzy elementami mapy. W tym celu dla każdej możliwej wartości sprawdzana
-     * jest różnica między ilością wystąpień jednego elementu względem innego. W ten soposób możemy próbować oszacować
-     * na ile nasz generator liczb losowych jest uczciwy. W przypadku idealnym różnica pomiędzy wartościami powinna
-     * być 0, co zonacza że wszystkie wartości wystąpiły taką samą liczbę razy.
-     * Przerwanie wątku powinno nastąpić najszybciej jak to tylko możliwe
-     *
-     * @param map
-     */
     public void maxRoznica(Map<String, Integer> map) {
         int maxDX = 0;
         System.out.println("maxRoznica: " + counter);
