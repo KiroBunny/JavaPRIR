80f3d1a6af93bfc0d6098fabec87441cdb464d87
Edyta Pałka <65342415+KiroBunny@users.noreply.github.com>
2020-12-18 11:52:22 +0100

    Update Licznik.java

--- watki/zad3/Licznik.java
+++ watki/zad3/Licznik.java
@@ -10,8 +10,3 @@ public class Licznik {
         return licznik;
     }
 }
-/* Jeśli nie korzystamy z synchronizacji wątków, to następuje problem zwiazany z dostępem do licznika w klasie Licznik. CHcemy aby po kolei każdy wątek inkrementował
- statyczną zmienną licznik, jednak bez synchronizacji wątki działają w tym samym czasie. Podczas gdy wątek1 pobierze kopie licznika do swojej pamięci inny wątek
- może zrobić to samo oraz też inkrementować, co finalnie daje, że dwa wątki działają na tym samym poprzednim stanie licznika. Nasza zmienna licznik, po zakończeniu programu,
- nie pokazuje poprawnego wyniku.
- */
