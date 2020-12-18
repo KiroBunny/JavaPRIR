b6bb0b24a1ab03bacdc388c84bff9d5fd6399172
Edyta Pałka <65342415+KiroBunny@users.noreply.github.com>
2020-12-18 11:50:05 +0100

    Update BreakingTask1ToDo.java

--- threads/breaking/BreakingTask1ToDo.java
+++ threads/breaking/BreakingTask1ToDo.java
@@ -4,17 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-/**
- * Zmodyfikuj metodę run w klasie BreakingTask1ToDo tak aby po wystąpieniu przerwaniu wątka, metoda run zakończyła swoją
- * pracę a jednocześnie wypisała na ekranie zawartość mapy
- */
 public class BreakingTask1ToDo implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         Thread t = new Thread(new BreakingTask1ToDo());
         t.start();
         Thread.sleep(500);
         t.interrupt();
-        //t.join();
         System.out.println("Koniec");
     }
 
