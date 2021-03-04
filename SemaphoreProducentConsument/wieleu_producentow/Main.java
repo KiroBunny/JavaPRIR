71648bb03402f41504c05782fbf0495ccbeac8ed
Edyta Pałka <65342415+KiroBunny@users.noreply.github.com>
2021-03-04 11:06:38 +0100

    Update Main.java

--- SemaphoreProducentConsument/wieleu_producentow/Main.java
+++ SemaphoreProducentConsument/wieleu_producentow/Main.java
@@ -1,23 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package SemaphoreProducentConsument.wieleu_producentow;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- *
- * @author Marcin
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) throws InterruptedException {
         Magazyn m = new MagazynImpl(3);
         List<Thread> th = new ArrayList<>();
