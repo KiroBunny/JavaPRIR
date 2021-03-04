69d52004ee6f41f7d2268ee0aa4d578a5dcd677c
Edyta Pałka <65342415+KiroBunny@users.noreply.github.com>
2021-03-04 11:08:50 +0100

    Update Producent.java

--- SemaphoreProducentConsument/wieleu_producentow/Producent.java
+++ SemaphoreProducentConsument/wieleu_producentow/Producent.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package SemaphoreProducentConsument.wieleu_producentow;
 import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- * @author Marcin
- */
 public class Producent extends Thread {
 
     private Magazyn magazyn;
@@ -25,7 +18,7 @@ public class Producent extends Thread {
                 synchronized (Producent.class) {
                     produkt = "" + counter;
                     counter++;
-                } //UWAGA tutaj ciągle może dojść do wyścigu ponieważ różne operacja magazyn.add() nie jest synchronizowana, więc inny wątek może dodać wcześniej niż nasz
+                } 
                 magazyn.add(produkt);
                 System.out.println("Wrzuciłem do magazynu: " + produkt);
                 Thread.sleep((int) (Math.random() * 10));
